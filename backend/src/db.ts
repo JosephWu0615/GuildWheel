0a762ea44b2dccbe90ac50ef25ad383659ffb285
Joseph <cccj9267@gmail.com>
2025-04-14 08:19:04 -0700

    try to deploy to GCP

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,16 @@
 import { Pool } from 'pg';
 
+// export const pool = new Pool({
+//   user: 'a2301133',
+//   host: 'localhost',
+//   database: 'factory_monitor',
+//   password: '',
+//   port: 5432,
+// });
+
 export const pool = new Pool({
-  user: 'a2301133',
-  host: 'localhost',
-  database: 'factory_monitor',
-  password: '',
-  port: 5432,
-});
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+})
